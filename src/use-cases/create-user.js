99f3b168b92ce22ae18c0f31d2f4fa33dceb72a7
Abimael <abimael.spdev@gmail.com>
2025-05-22 22:01:02 -0400

    feat: add CreateUserUseCase

---
+++ src/use-cases/create-user.js
@@ -0,0 +1,24 @@
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
+
+import { PostgresCreateUserRepository } from '../repositories/postgres-db/create-user.js';
+
+export class CreateUserUseCase {
+    async execut(createUserParams) {
+        // TODO: verificar se o e-mail já está em uso
+
+        const userId = uuidv4();
+        const hashedPassword = await bcrypt.hash(createUserParams, 10);
+
+        const user = {
+            ...createUserParams,
+            id: userId,
+            password: hashedPassword,
+        };
+
+        const postgresCreateUserRepository = new PostgresCreateUserRepository();
+        const createdUser = await postgresCreateUserRepository.execute(user);
+
+        return createdUser;
+    }
+}
