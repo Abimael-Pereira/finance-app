8377a32fe5f8b4c0fba389593035bbb3cbcabdaa
Abimael <abimael.spdev@gmail.com>
2025-05-25 01:53:49 -0400

    feat: add UpdateUserUseCase

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,41 @@
+import bcrypt from 'bcrypt';
+
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres-db/get-user-by-email.js';
+import { EmailAlreadyInUseError } from '../errors/user.js';
+import { PostgresUpdateUserRepository } from '../repositories/postgres-db/update-user';
+
+export class UpdateUserUseCase {
+    async execute(userId, updateUserParams) {
+        if (updateUserParams.email) {
+            const postgresGetUserByEmailRepository =
+                new PostgresGetUserByEmailRepository();
+
+            const userWithProvidedEmail =
+                postgresGetUserByEmailRepository.execute(
+                    updateUserParams.email,
+                );
+
+            if (userWithProvidedEmail) {
+                throw new EmailAlreadyInUseError(updateUserParams.email);
+            }
+        }
+
+        const user = {
+            ...updateUserParams,
+        };
+
+        if (updateUserParams.password) {
+            const hashedPassword = await bcrypt.hash(
+                updateUserParams.password,
+                10,
+            );
+
+            user.password = hashedPassword;
+        }
+
+        const postgresUpdateUserRepository = new PostgresUpdateUserRepository();
+        const updatedUser = await postgresUpdateUserRepository(userId, user);
+
+        return updatedUser;
+    }
+}
