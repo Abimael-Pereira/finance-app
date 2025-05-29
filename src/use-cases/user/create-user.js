414133334b3f079e3ca9db765fc2c36fe85f46e7
Abimael <abimael.spdev@gmail.com>
2025-05-28 20:53:56 -0400

    feat: add dependency injection to CreateUserController

--- src/use-cases/user/create-user.js
+++ src/use-cases/user/create-user.js
@@ -1,20 +1,16 @@
 import { v4 as uuidv4 } from 'uuid';
 import bcrypt from 'bcrypt';
 
-import { PostgresGetUserByEmailRepository } from '../../repositories/postgres-db/index.js';
 import { EmailAlreadyInUseError } from '../../errors/user.js';
 
 export class CreateUserUseCase {
-    constructor(createUserRepository) {
+    constructor(createUserRepository, getUserByEmailRepository) {
         this.createUserRepository = createUserRepository;
+        this.getUserByEmailRepository = getUserByEmailRepository;
     }
     async execute(createUserParams) {
-        const postgresGetUserByEmailRepository =
-            new PostgresGetUserByEmailRepository();
         const userWithProvidedEmail =
-            await postgresGetUserByEmailRepository.execute(
-                createUserParams.email,
-            );
+            await this.getUserByEmailRepository.execute(createUserParams.email);
 
         if (userWithProvidedEmail) {
             throw new EmailAlreadyInUseError(createUserParams.email);
