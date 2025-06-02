a4745bd34576994565e0bdeb24c4c54fa637e32c
Abimael <abimael.spdev@gmail.com>
2025-06-02 09:55:36 -0400

    feat: add GetUserBalanceController

---
+++ src/controllers/user/get-user-balance.js
@@ -0,0 +1,38 @@
+import {
+    checkIfIdIsValid,
+    invalidIdResponse,
+    ok,
+    serverError,
+    userNotFoundResponse,
+} from '../helpers/index.js';
+import { UserNotFoundError } from '../../errors/user.js';
+
+export class GetUserBalanceController {
+    constructor(getUserBalanceUseCase) {
+        this.getUserBalanceUseCase = getUserBalanceUseCase;
+    }
+
+    async execute(httpRequest) {
+        try {
+            const userId = httpRequest.params.userId;
+            const userIdIsValid = checkIfIdIsValid(userId);
+
+            if (!userIdIsValid) {
+                return invalidIdResponse();
+            }
+
+            const balance = this.getUserBalanceUseCase.execute(
+                httpRequest.params,
+            );
+
+            return ok(balance);
+        } catch (error) {
+            if (error instanceof UserNotFoundError) {
+                return userNotFoundResponse();
+            }
+
+            console.log(error);
+            return serverError();
+        }
+    }
+}
