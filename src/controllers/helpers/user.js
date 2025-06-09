098f166abd7a55656a9851ec059ca9671352bea8
Abimael <abimael.spdev@gmail.com>
2025-06-09 10:56:58 -0400

    refactor: remove unused code

--- src/controllers/helpers/user.js
+++ src/controllers/helpers/user.js
@@ -1,21 +1,7 @@
-import { badRequest, notFound } from './index.js';
+import { notFound } from './index.js';
 
-export const invalidPasswordResponse = () =>
-    badRequest({
-        message: 'Password must be at least 6 characters',
-    });
-
-export const emailAlreadyInUseResponse = () =>
-    badRequest({
-        message: 'Invalid e-mail. Please provide a valid one.',
-    });
-
-export const emailIsNotValidResponse = () =>
-    badRequest({
-        message: 'Invalid e-mail. Please provide a valid one.',
-    });
-
-export const userNotFoundResponse = () =>
-    notFound({
+export const userNotFoundResponse = () => {
+    return notFound({
         message: 'User not found.',
     });
+};
