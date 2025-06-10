8127bbbf175a8fc48225e7eddb7eaa4d3f88f815
Abimael <abimael.spdev@gmail.com>
2025-06-10 12:23:59 -0400

    test: ensure GetUserByIdUseCase get user successfully

---
+++ src/use-cases/user/get-user-by-id.test.js
@@ -0,0 +1,36 @@
+import { faker } from '@faker-js/faker';
+import { GetUserByIdUseCase } from './get-user-by-id';
+
+describe('GetUserById', () => {
+    const user = {
+        id: faker.string.uuid(),
+        email: faker.internet.email(),
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        password: faker.internet.password(),
+    };
+
+    class GetUserByIdRepositoryStub {
+        async execute() {
+            return user;
+        }
+    }
+
+    const makeSut = () => {
+        const getUserByIdRepository = new GetUserByIdRepositoryStub();
+        const getUserByIdUseCase = new GetUserByIdUseCase(
+            getUserByIdRepository,
+        );
+
+        return { getUserByIdUseCase, getUserByIdRepository };
+    };
+
+    it('should get a user by id successfully', async () => {
+        const { getUserByIdUseCase } = makeSut();
+
+        const result = await getUserByIdUseCase.execute(user.id);
+
+        expect(result).toBeTruthy();
+        expect(result).toEqual(user);
+    });
+});
