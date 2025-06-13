dfa7519847b547c21b0915c0363fe84761647f26
Abimael <abimael.spdev@gmail.com>
2025-06-13 02:12:12 -0400

    test: ensure GetUserByEmailRepository calls Prisma with correct params

--- src/repositories/postgres-db/user/get-user-by-email.test.js
+++ src/repositories/postgres-db/user/get-user-by-email.test.js
@@ -1,15 +1,29 @@
-import { user } from '../../../tests';
+import { user as fakerUser } from '../../../tests';
 import { prisma } from '../../../../prisma/prisma';
 import { PostgresGetUserByEmailRepository } from './get-user-by-email';
 
 describe('GetUserByEmailRepository', () => {
     it('should get user by email on db', async () => {
-        await prisma.user.create({ data: user });
+        await prisma.user.create({ data: fakerUser });
 
         const sut = new PostgresGetUserByEmailRepository();
 
-        const result = await sut.execute(user.email);
+        const result = await sut.execute(fakerUser.email);
 
-        expect(result).toStrictEqual(user);
+        expect(result).toStrictEqual(fakerUser);
+    });
+
+    it('should call Prisma with correct params', async () => {
+        const sut = new PostgresGetUserByEmailRepository();
+
+        const prismaSpy = jest.spyOn(prisma.user, 'findUnique');
+
+        await sut.execute(fakerUser.email);
+
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                email: fakerUser.email,
+            },
+        });
     });
 });
