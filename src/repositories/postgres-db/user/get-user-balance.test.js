efc9b8c7e7e2af523fe59f8253fb5a379c971ae2
Abimael <abimael.spdev@gmail.com>
2025-06-12 22:33:11 -0400

    fix: ensure transactions are deleted first on jest setup file

---
+++ src/repositories/postgres-db/user/get-user-balance.test.js
@@ -0,0 +1,66 @@
+import { user as fakeUser } from '../../../tests';
+import { prisma } from '../../../../prisma/prisma';
+import { PostgresGetUserBalanceRepository } from './get-user-balance';
+import { faker } from '@faker-js/faker';
+
+describe('GetUserBalanceRepository', () => {
+    it('should get user balance on db', async () => {
+        await prisma.user.create({ data: fakeUser });
+
+        await prisma.transaction.createMany({
+            data: [
+                {
+                    userId: fakeUser.id,
+                    name: faker.string.sample(),
+                    date: faker.date.recent(),
+                    amount: 5000,
+                    type: 'EARNING',
+                },
+                {
+                    userId: fakeUser.id,
+                    name: faker.string.sample(),
+                    date: faker.date.recent(),
+                    amount: 5000,
+                    type: 'EARNING',
+                },
+                {
+                    userId: fakeUser.id,
+                    name: faker.string.sample(),
+                    date: faker.date.recent(),
+                    amount: 1000,
+                    type: 'EXPENSE',
+                },
+                {
+                    userId: fakeUser.id,
+                    name: faker.string.sample(),
+                    date: faker.date.recent(),
+                    amount: 1000,
+                    type: 'EXPENSE',
+                },
+                {
+                    userId: fakeUser.id,
+                    name: faker.string.sample(),
+                    date: faker.date.recent(),
+                    amount: 3000,
+                    type: 'INVESTMENT',
+                },
+                {
+                    userId: fakeUser.id,
+                    name: faker.string.sample(),
+                    date: faker.date.recent(),
+                    amount: 3000,
+                    type: 'INVESTMENT',
+                },
+            ],
+        });
+
+        const sut = new PostgresGetUserBalanceRepository();
+
+        const result = await sut.execute(fakeUser.id);
+
+        expect(result.earnings.toString()).toBe('10000');
+        expect(result.expenses.toString()).toBe('2000');
+        expect(result.invesments.toString()).toBe('6000');
+        expect(result.balance.toString()).toBe('2000');
+    });
+});
