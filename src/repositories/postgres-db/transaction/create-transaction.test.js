01989d98e83d4dca0e439cf41b698932c5d5a5c6
Abimael <abimael.spdev@gmail.com>
2025-06-14 01:43:02 -0400

    test: ensure CreateTransactionRepository create a transaction on db

---
+++ src/repositories/postgres-db/transaction/create-transaction.test.js
@@ -0,0 +1,31 @@
+import dayjs from 'dayjs';
+import { prisma } from '../../../../prisma/prisma';
+import { transaction, user as fakeUser } from '../../../tests/';
+import { PostgresCreateTransactionRepository } from './create-transaction';
+
+describe('CreateTransactionRepository', () => {
+    it('should create a transaction on db', async () => {
+        const user = await prisma.user.create({ data: fakeUser });
+        const sut = new PostgresCreateTransactionRepository();
+
+        const result = await sut.execute({ ...transaction, userId: user.id });
+
+        expect({
+            ...result,
+            amount: result.amount.toString(),
+            date: undefined,
+        }).toStrictEqual({
+            ...transaction,
+            amount: transaction.amount.toString(),
+            userId: user.id,
+            date: undefined,
+        });
+        expect(dayjs(result.date).daysInMonth()).toBe(
+            dayjs(transaction.date).daysInMonth(),
+        );
+        expect(dayjs(result.date).month()).toBe(
+            dayjs(transaction.date).month(),
+        );
+        expect(dayjs(result.date).year()).toBe(dayjs(transaction.date).year());
+    });
+});
