34d33330772520bf72e2b8a7e62ed14ace9d9685
Abimael <abimael.spdev@gmail.com>
2025-06-16 16:40:03 -0400

    test: ensure CreateTransactionRepository call prisma with correct params and throw if Prisma throws

--- src/repositories/postgres-db/transaction/create-transaction.test.js
+++ src/repositories/postgres-db/transaction/create-transaction.test.js
@@ -1,14 +1,17 @@
 import dayjs from 'dayjs';
 import { prisma } from '../../../../prisma/prisma';
-import { transaction, user as fakeUser } from '../../../tests/';
+import { transaction, user } from '../../../tests/';
 import { PostgresCreateTransactionRepository } from './create-transaction';
 
 describe('CreateTransactionRepository', () => {
     it('should create a transaction on db', async () => {
-        const user = await prisma.user.create({ data: fakeUser });
+        await prisma.user.create({ data: user });
         const sut = new PostgresCreateTransactionRepository();
 
-        const result = await sut.execute({ ...transaction, userId: user.id });
+        const result = await sut.execute({
+            ...transaction,
+            userId: user.id,
+        });
 
         expect({
             ...result,
@@ -27,5 +30,30 @@ describe('CreateTransactionRepository', () => {
             dayjs(transaction.date).month(),
         );
         expect(dayjs(result.date).year()).toBe(dayjs(transaction.date).year());
+    });
+
+    it('should call Prisma with correct params', async () => {
+        await prisma.user.create({ data: user });
+        const sut = new PostgresCreateTransactionRepository();
+
+        const prismaSpy = jest.spyOn(prisma.transaction, 'create');
+
+        await sut.execute({ ...transaction, userId: user.id });
+
+        expect(prismaSpy).toHaveBeenCalledWith({
+            data: { ...transaction, userId: user.id },
+        });
+    });
+
+    it('should throws if Prisma throws', async () => {
+        const sut = new PostgresCreateTransactionRepository();
+
+        jest.spyOn(prisma.transaction, 'create').mockRejectedValueOnce(
+            new Error(),
+        );
+
+        const result = sut.execute(transaction);
+
+        await expect(result).rejects.toThrow();
     });
 });
