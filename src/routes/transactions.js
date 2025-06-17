3a52ba71911971b4f12dcbccda3336ba77144193
Abimael <abimael.spdev@gmail.com>
2025-06-17 19:51:38 -0400

    refactor: move transactions routes to separate file

---
+++ src/routes/transactions.js
@@ -0,0 +1,46 @@
+import {
+    makeCreateTransactionController,
+    makeDeleteTransactionController,
+    makeGetTransactionsByIdController,
+    makeUpdateTransactionController,
+} from '../factories/controllers/transaction.js';
+import { Router } from 'express';
+
+export const transactionsRouter = Router();
+
+transactionsRouter.post('/', async (request, response) => {
+    const createTransactionController = makeCreateTransactionController();
+
+    const { statusCode, body } =
+        await createTransactionController.execute(request);
+
+    response.status(statusCode).send(body);
+});
+
+transactionsRouter.get('/', async (request, response) => {
+    const getTransactionsByUserIdController =
+        makeGetTransactionsByIdController();
+
+    const { statusCode, body } =
+        await getTransactionsByUserIdController.execute(request);
+
+    response.status(statusCode).send(body);
+});
+
+transactionsRouter.patch('/:transactionId', async (request, response) => {
+    const updateTransactionController = makeUpdateTransactionController();
+
+    const { statusCode, body } =
+        await updateTransactionController.execute(request);
+
+    response.status(statusCode).send(body);
+});
+
+transactionsRouter.delete('/:transactionId', async (request, response) => {
+    const deleteTransactionController = makeDeleteTransactionController();
+
+    const { statusCode, body } =
+        await deleteTransactionController.execute(request);
+
+    response.status(statusCode).send(body);
+});
