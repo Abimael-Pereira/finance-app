df559374b2c6011f94698373679e750f27c00547
Abimael <abimael.spdev@gmail.com>
2025-06-17 00:50:29 -0400

    test: ensure DeleteTransactionRepository throw if Prisma throw

--- src/repositories/postgres-db/transaction/delete-transaction.test.js
+++ src/repositories/postgres-db/transaction/delete-transaction.test.js
@@ -2,6 +2,7 @@ import dayjs from 'dayjs';
 import { prisma } from '../../../../prisma/prisma.js';
 import { transaction, user } from '../../../tests/';
 import { PostgresDeleteTransactionRepository } from './delete-transaction';
+import { TransactionNotFoundError } from '../../../errors/';
 
 describe('DeleteTransactionRepository', () => {
     it('should delete a transaction on db', async () => {
@@ -34,16 +35,42 @@ describe('DeleteTransactionRepository', () => {
     });
 
     it('shoul call prisma with correct params', async () => {
+        await prisma.user.create({ data: user });
+        await prisma.transaction.create({
+            data: { ...transaction, userId: user.id },
+        });
         const sut = new PostgresDeleteTransactionRepository();
 
-        const result = jest.spyOn(prisma.transaction, 'delete');
+        const prismaSpy = jest.spyOn(prisma.transaction, 'delete');
 
         await sut.execute(transaction.id);
 
-        await expect(result).toHaveBeenCalledWith({
+        expect(prismaSpy).toHaveBeenCalledWith({
             where: {
                 id: transaction.id,
             },
         });
+    });
+
+    it('should throw generic error if Prisma throws generic error', async () => {
+        const sut = new PostgresDeleteTransactionRepository();
+
+        jest.spyOn(prisma.transaction, 'delete').mockRejectedValueOnce(
+            new Error(),
+        );
+
+        const result = sut.execute(transaction.id);
+
+        await expect(result).rejects.toThrow();
+    });
+
+    it('should throw TransactionNotFoundError if transaction not found', async () => {
+        const sut = new PostgresDeleteTransactionRepository();
+
+        const result = sut.execute(transaction.id);
+
+        await expect(result).rejects.toThrow(
+            new TransactionNotFoundError(transaction.id),
+        );
     });
 });
