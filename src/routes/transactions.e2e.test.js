974294e34f6ae2ac3d61f5365eec92aaf3e733f6
Abimael <abimael.spdev@gmail.com>
2025-06-19 01:14:43 -0400

    test: ensure PATCH /api/transactions/:transactionId should return 200 when updating a transaction successfully

---
+++ src/routes/transactions.e2e.test.js
@@ -0,0 +1,110 @@
+import request from 'supertest';
+import { app } from '../app.js';
+import { user, transactionWithoutId } from '../tests/index.js';
+import dayjs from 'dayjs';
+import dayjsPluginUTC from 'dayjs-plugin-utc';
+
+dayjs.extend(dayjsPluginUTC);
+
+describe('TransactionsRoutes E2E Tests', () => {
+    it('POST /api/transactions should return 201 when creating a transaction successfully', async () => {
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({ ...user, id: undefined });
+
+        const response = await request(app)
+            .post('/api/transactions')
+            .send({ ...transactionWithoutId, userId: createdUser.id });
+
+        expect(response.status).toBe(201);
+        expect(response.body.userId).toBe(createdUser.id);
+        expect(response.body.type).toBe(transactionWithoutId.type);
+        expect(response.body.amount).toBe(String(transactionWithoutId.amount));
+        expect(response.body.name).toBe(transactionWithoutId.name);
+        expect(dayjs(response.body).date()).toBe(
+            dayjs(transactionWithoutId).date(),
+        );
+        expect(dayjs(response.body).month()).toBe(
+            dayjs(transactionWithoutId).month(),
+        );
+        expect(dayjs(response.body).year()).toBe(
+            dayjs(transactionWithoutId).year(),
+        );
+    });
+
+    it('GET /api/transactions?userId should return 200 when fetching transactions successfully', async () => {
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({ ...user, id: undefined });
+
+        const transactionParams = {
+            ...transactionWithoutId,
+            userId: createdUser.id,
+        };
+
+        const createdTransaction = await request(app)
+            .post('/api/transactions')
+            .send(transactionParams);
+
+        const response = await request(app).get('/api/transactions').query({
+            userId: createdUser.id,
+        });
+
+        expect(response.status).toBe(200);
+        expect(response.body[0].id).toBe(createdTransaction.body.id);
+        expect(response.body[0].userId).toBe(createdUser.id);
+        expect(response.body[0].type).toBe(transactionParams.type);
+        expect(response.body[0].amount).toBe(String(transactionParams.amount));
+        expect(response.body[0].name).toBe(transactionParams.name);
+        expect(dayjs(response.body[0].date).date()).toBe(
+            dayjs(transactionParams.date).date(),
+        );
+        expect(dayjs(response.body[0].date).month()).toBe(
+            dayjs(transactionParams.date).month(),
+        );
+        expect(dayjs(response.body[0].date).year()).toBe(
+            dayjs(transactionParams.date).year(),
+        );
+    });
+
+    it('PATCH /api/transactions/:transactionId should return 200 when updating a transaction successfully', async () => {
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({ ...user, id: undefined });
+
+        const transactionParams = {
+            ...transactionWithoutId,
+            userId: createdUser.id,
+        };
+
+        const { body: createdTransaction } = await request(app)
+            .post('/api/transactions')
+            .send(transactionParams);
+
+        const updatedTransaction = {
+            ...transactionWithoutId,
+            userId: undefined, // userId should not be updated
+            name: 'Updated Transaction',
+        };
+
+        const response = await request(app)
+            .patch(`/api/transactions/${createdTransaction.id}`)
+            .send(updatedTransaction);
+
+        expect(response.status).toBe(200);
+        expect(response.body.id).toBe(createdTransaction.id);
+        expect(response.body.userId).toBe(createdUser.id);
+        expect(response.body.type).toBe(updatedTransaction.type);
+        expect(response.body.amount).toBe(String(updatedTransaction.amount));
+        expect(response.body.name).toBe(updatedTransaction.name);
+        expect(dayjs(response.body.date).date()).toBe(
+            dayjs(updatedTransaction.date).date(),
+        );
+        expect(dayjs(response.body.date).month()).toBe(
+            dayjs(updatedTransaction.date).month(),
+        );
+        expect(dayjs(response.body.date).year()).toBe(
+            dayjs(updatedTransaction.date).year(),
+        );
+    });
+});
