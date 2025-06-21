88832911ec1c787afee9cca85842a19ba4e39762
Abimael <abimael.spdev@gmail.com>
2025-06-20 23:27:15 -0400

    ci: fix test run on gh actions

--- src/routes/transactions.e2e.test.js
+++ src/routes/transactions.e2e.test.js
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import { app } from '../app.js';
+import { app } from '../app.cjs';
 import { user, transactionWithoutId } from '../tests/index.js';
 import dayjs from 'dayjs';
 import dayjsPluginUTC from 'dayjs-plugin-utc';
